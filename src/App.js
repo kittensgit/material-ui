7fd118bc3722b9c613343e7d3524a9a68b98f4f1
Nika <nikaonime@gmail.com>
2023-08-04 07:31:50 +0300

    Typography, Box, createTheme, makeStyles,useStyles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {
     AppBar,
+    Box,
+    Button,
     Container,
     IconButton,
     Toolbar,
@@ -7,19 +9,55 @@ import {
 } from '@mui/material';
 import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider, makeStyles } from '@mui/styles';
+import { createTheme } from '@mui/material/styles';
+import { spacing } from '@mui/system';
+
+const theme = createTheme();
+console.log(theme);
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: spacing(1),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function App() {
+    const classes = useStyles();
     return (
-        <AppBar position="fixed">
-            <Container fixed>
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6">Website MUI</Typography>
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <ThemeProvider theme={theme}>
+            <AppBar position="fixed">
+                <Container fixed>
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            className={classes.menuButton}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Website MUI
+                        </Typography>
+                        <Box mr={3}>
+                            <Button color="inherit" variant="outlined">
+                                Log In
+                            </Button>
+                        </Box>
+                        <Button color="secondary" variant="contained">
+                            Sign Up
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
     );
 }
 
